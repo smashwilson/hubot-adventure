22ee1dfe8c43bcf50ab0b5b9c65e3ffe625060ac
Ash Wilson <smashwilson@github.com>
2017-12-27 22:57:27 -0500

    Grammar and AST nodes for literals and simple statements

---
+++ src/gnomish/ast.js
@@ -0,0 +1,119 @@
+class ExprListNode {
+  constructor (exprs) {
+    this.exprs = exprs
+  }
+
+  getExprs () {
+    return this.exprs
+  }
+
+  visitBy (visitor) {
+    return visitor.visitExprList(this)
+  }
+}
+
+class IfNode {
+  constructor ({condition, thenb, elseb}) {
+    this.condition = condition
+    this.thenb = thenb
+    this.elseb = elseb || new BlockNode({})
+  }
+
+  getCondition () { return this.condition }
+
+  getThen () { return this.thenb }
+
+  getElse () { return this.elseb }
+
+  visitBy (visitor) {
+    return visitor.visitIf(this)
+  }
+}
+
+class BlockNode {
+  constructor ({args, body}) {
+    this.args = args || []
+    this.body = body || new ExprListNode([])
+  }
+
+  getArgs () {
+    return this.args
+  }
+
+  getBody () {
+    return this.body
+  }
+
+  visitBy (visitor) {
+    return visitor.visitBlock(this)
+  }
+}
+
+class ArgNode {
+  constructor ({name, type, def}) {
+    this.name = name
+    this.type = type
+    this.def = def
+  }
+
+  getName () { return this.name }
+
+  getType () { return this.type }
+
+  getDefault () { return this.def }
+
+  visitBy (visitor) {
+    return visitor.visitArg(this)
+  }
+}
+
+class IntNode {
+  constructor ({minus, digits}) {
+    this.value = parseInt((minus || '') + digits.join(''), 10)
+  }
+
+  visitBy (visitor) {
+    return visitor.visitInt(this)
+  }
+}
+
+class RealNode {
+  constructor ({minus, whole, fraction}) {
+    this.value = parseFloat((minus || '') + whole.join('') + '.' + fraction.join(''))
+  }
+
+  visitBy (visitor) {
+    return visitor.visitReal(this)
+  }
+}
+
+class StringNode {
+  constructor ({chars}) {
+    this.value = chars.join('')
+  }
+
+  visitBy (visitor) {
+    return visitor.visitString(this)
+  }
+}
+
+class VarNode {
+  constructor ({name}) {
+    this.name = name
+  }
+
+  visitBy (visitor) {
+    return visitor.visitVar(this)
+  }
+}
+
+module.exports = {
+  ExprListNode,
+  IfNode,
+  BlockNode,
+  ArgNode,
+  IntNode,
+  RealNode,
+  StringNode,
+  VarNode
+}
---
+++ src/gnomish/gnomish.pegjs
@@ -0,0 +1,66 @@
+// PEG grammer for the Gnomish language
+
+{
+  const {ExprListNode, IfNode, IntNode, RealNode, StringNode, BlockNode, ArgNode, VarNode} = require('./ast')
+}
+
+// Expressions ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+exprlist
+  = first:expr rest:( _ ( ';' / '\n' ) _ each:expr _ { return each } )*
+    { return new ExprListNode([first, ...rest]) }
+
+expr "expression"
+  = if
+  / literal
+
+if
+  = 'if' _ condition:block _ 'then' _ thenb:block elseb:( _ 'else' _ e:block { return e } )?
+    { return new IfNode({condition, thenb, elseb}) }
+
+// Literals ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+literal
+  = real
+  / int
+  / string
+  / var
+  / block
+
+real
+  = minus:'-'? whole:[0-9]+ '.' fraction:[0-9]+
+    { return new RealNode({minus, whole, fraction}) }
+
+int "integer"
+  = minus:'-'? digits:[0-9]+
+    { return new IntNode({minus, digits}) }
+
+string
+  = '"' chars:( '\\' char:. { return char } / [^"] )* '"'
+    { return new StringNode({chars}) }
+
+var
+  = name:identifier
+    { return new VarNode({name}) }
+
+identifier
+  = $ ( [a-zA-Z] [0-9a-zA-Z_]* )
+
+// Blocks
+
+block
+  = '{' _ args:blockargs? _ body:exprlist? _ '}'
+    { return new BlockNode({args, body}) }
+
+blockargs "block arguments"
+  = first:blockarg rest:(_ ',' _ arg:blockarg { return arg } )* _ '|'
+    { return [first, ...rest] }
+
+blockarg "block argument"
+  = name:identifier type:(_ ':' _ t:expr { return t })? def:(_ '=' _ d:expr { return d })?
+    { return new ArgNode({name, type, def}) }
+
+// Whitespace /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+_ "optional whitespace"
+  = [ \t\r\n]*
