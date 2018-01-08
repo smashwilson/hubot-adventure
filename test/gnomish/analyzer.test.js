8f00dce6cdc8b16d80f37ea4bcd6cb9322184027
Ash Wilson <smashwilson@gmail.com>
2018-01-08 10:00:38 -0500

    Type assignment tests for ArgNodes and IfNodes

--- test/gnomish/analyzer.test.js
+++ test/gnomish/analyzer.test.js
@@ -9,18 +9,22 @@ const {MethodRegistry} = require('../../src/gnomish/methodregistry')
 
 describe('Analyzer', function () {
   let st, mr, analyzer
-  let tInt, tReal, tString, tType
+  let tInt, tReal, tString, tBool, tBlock, tType
 
   beforeEach(function () {
     tInt = makeType('Int')
     tReal = makeType('Real')
     tString = makeType('String')
+    tBool = makeType('Bool')
+    tBlock = makeType('Block')
     tType = makeType('Type')
 
     st = new SymbolTable()
     st.put('Int', new StaticEntry(tType, tInt))
     st.put('Real', new StaticEntry(tType, tReal))
     st.put('String', new StaticEntry(tType, tString))
+    st.put('Bool', new StaticEntry(tType, tBool))
+    st.put('Block', new StaticEntry(tType, tBlock))
     st.put('Type', new StaticEntry(tType, tType))
 
     mr = new MethodRegistry()
@@ -57,6 +61,65 @@ describe('Analyzer', function () {
       analyzer.visit(root.node)
 
       assert.strictEqual(root.node.getType(), tString)
+    })
+
+    describe('ArgNode', function () {
+      it('assigns a type from a type annotation', function () {
+        const root = parse('{ x: Int | x }')
+        analyzer.visit(root.node)
+
+        const blockNode = root.node.getLastExpr()
+        const argNode = blockNode.getArgs()[0]
+
+        assert.strictEqual(argNode.getType(), tInt)
+      })
+
+      it('infers a type based on a default value', function () {
+        const root = parse('{ x = "yes" | x }')
+        console.log(root.sexp())
+        analyzer.visit(root.node)
+
+        const blockNode = root.node.getLastExpr()
+        const argNode = blockNode.getArgs()[0]
+
+        assert.strictEqual(argNode.getType(), tString)
+      })
+
+      it('fails when a type annotation and default value are inconsistent', function () {
+        const root = parse('{ x : Bool = "yes" | 7 }')
+        assert.throws(() => analyzer.visit(root.node), /Types "Bool" and "String" do not match/)
+      })
+
+      it('may be an unbound type parameter', function () {
+        const root = parse("{ x : 'A | x }")
+        analyzer.visit(root.node)
+
+        const blockNode = root.node.getLastExpr()
+        const argNode = blockNode.getArgs()[0]
+        const argType = argNode.getType()
+
+        assert.isTrue(argType.isParam())
+        assert.equal(argType.getName(), "'A")
+      })
+    })
+
+    describe('IfNode', function () {
+      it('ensures that the condition clause of an IfNode evaluates to a Bool', function () {
+        const success = parse('if {true} then {"no"} else {"uh"}')
+        analyzer.visit(success)
+
+        const failure = parse('if {6} then {3} else {"no"}')
+        assert.throws(() => analyzer.visit(failure.node), /Types "Int" and "Bool" do not match/)
+      })
+
+      it('ensures that the "then" and "else" branches of an IfNode are consistent', function () {
+        const success = parse('if {true} then {3} else {4}')
+        analyzer.visit(success.node)
+        assert.strictEqual(success.node.getType(), tInt)
+
+        const failure = parse('if {true} then {3} else {"no"}')
+        assert.throws(() => analyzer.visit(failure.node), /Types "Int" and "String" do not match/)
+      })
     })
   })
 })
