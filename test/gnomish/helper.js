fdc80d0d2f9dc52c0ba6f77cf65d756d9601d380
Ash Wilson <smashwilson@github.com>
2018-01-08 11:51:52 -0500

    Extract a traced parse helper function

--- test/gnomish/helper.js
+++ test/gnomish/helper.js
@@ -3,25 +3,28 @@ const Tracer = require('pegjs-backtrace')
 
 const {parse} = require('../../src/gnomish')
 
-function assertSexp (text, expected, shouldParse = true) {
+function tracedParse (text, shouldParse = true) {
   const tracer = new Tracer(text, {
     showTrace: process.env.GNOMISH_TRACE === 'on'
   })
-  let actual = ''
   try {
-    actual = parse(text, {tracer}).sexp()
+    const root = parse(text, {tracer})
     if (!shouldParse) {
       assert.fail(null, null, `Should not parse ${text}`)
     }
+    return root
   } catch (e) {
     if (shouldParse) {
       console.error(tracer.getBacktraceString())
       assert.fail(null, null, `Unable to parse expression: ${e.message}`)
     }
   }
+}
 
+function assertSexp (text, expected, shouldParse = true) {
+  const actual = tracedParse(text, shouldParse)
   const normalize = str => str.trim().replace(/\s+/g, ' ')
   assert.equal(normalize(actual), normalize(expected))
 }
 
-module.exports = {assertSexp}
+module.exports = {parse: tracedParse, assertSexp}
