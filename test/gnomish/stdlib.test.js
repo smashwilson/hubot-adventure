a312b6cdd5ec8a5ae499660e329df4a9d6e59ec4
Ash Wilson <smashwilson@github.com>
2018-01-18 15:41:20 -0500

    Test framework for exercising the standard library

---
+++ test/gnomish/stdlib.test.js
@@ -0,0 +1,30 @@
+/* eslint-env mocha */
+
+const fs = require('fs-extra')
+const path = require('path')
+const assert = require('./lib/assertion')
+const {parse} = require('./helper')
+const stdlib = require('../../src/gnomish/stdlib')
+const {SymbolTable} = require('../../src/gnomish/symboltable')
+const {MethodRegistry} = require('../../src/gnomish/methodregistry')
+
+const symbolTable = new SymbolTable(Symbol('global'))
+const methodRegistry = new MethodRegistry()
+stdlib.register(symbolTable, methodRegistry)
+assert.register(symbolTable, methodRegistry)
+
+describe('Gnomish standard library', function () {
+  async function executeTestFile (testPath) {
+    const testSource = await fs.readFile(testPath, {encoding: 'utf8'})
+    parse(testSource).analyze(symbolTable, methodRegistry).interpret()
+  }
+
+  const testDir = path.join(__dirname, 'stdlib')
+  const testFiles = fs.readdirSync(testDir).filter(each => each.endsWith('.gn'))
+
+  for (const testFile of testFiles) {
+    it(`${testFile} passes`, async function () {
+      await executeTestFile(path.join(testDir, testFile))
+    })
+  }
+})
