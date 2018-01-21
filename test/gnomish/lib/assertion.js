e828080720a884d509ce143ea5352da67fbd8e2a
Ash Wilson <smashwilson@gmail.com>
2018-01-21 11:13:55 -0500

    Call a Gnomish == method in assert.equal

--- test/gnomish/lib/assertion.js
+++ test/gnomish/lib/assertion.js
@@ -11,17 +11,39 @@ module.exports = {
 
     symbolTable.setStatic('assert', t.Assert, Symbol('assert'))
 
-    methodRegistry.register(t.Assert, 'isTrue', [t.Bool], t.Option, (_, cond) => {
-      assert.isTrue(cond)
-      return none
-    })
-    methodRegistry.register(t.Assert, 'isFalse', [t.Bool], t.Option, (_, cond) => {
-      assert.isFalse(cond)
-      return none
-    })
-    methodRegistry.register(t.Assert, 'equal', [tA, tA], t.Option, (_, lhs, rhs) => {
-      assert.strictEqual(lhs, rhs)
-      return none
-    })
+    methodRegistry.register(
+      t.Assert, 'isTrue', [t.Bool], t.Option,
+      (_, cond) => {
+        assert.isTrue(cond)
+        return none
+      })
+
+    methodRegistry.register(
+      t.Assert, 'isFalse', [t.Bool], t.Option,
+      (_, cond) => {
+        assert.isFalse(cond)
+        return none
+      })
+
+    methodRegistry.register(
+      t.Assert, 'equal', [tA, tA], t.Option,
+      ({astNode, interpreter}, lhs, rhs) => {
+        const lType = astNode.getArgs()[0].getType()
+        const rType = astNode.getArgs()[0].getType()
+
+        const m = methodRegistry.lookup(symbolTable, lType, '==', [rType])
+        const result = m.getCallback()({
+          receiver: lhs,
+          selector: '==',
+          interpreter,
+          astNode: null
+        }, rhs)
+
+        if (!result) {
+          const message = `${lhs}:${lType} and ${rhs}:${rType} are not equal`
+          assert.fail(lhs, rhs, message, '==')
+        }
+        return none
+      })
   }
 }
