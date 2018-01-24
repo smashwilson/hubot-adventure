f5322b371fba53fc086c0937795e47d34b102633
Ash Wilson <smashwilson@github.com>
2018-01-23 21:54:30 -0500

    String methods

--- src/gnomish/stdlib/string.js
+++ src/gnomish/stdlib/string.js
@@ -4,6 +4,27 @@ module.exports = {
   },
 
   registerMethods (t, symbolTable, methodRegistry) {
-    //
+    methodRegistry.register(
+      t.String, 'length', [], t.Int,
+      ({receiver}) => receiver.length)
+
+    methodRegistry.register(
+      t.String, 'empty', [], t.Bool,
+      ({receiver}) => receiver.length === 0)
+
+    const substringBody = ({receiver}, start, length) => {
+      return receiver.substr(start, length)
+    }
+
+    methodRegistry.register(
+      t.String, 'substring', [t.Int, t.Int], t.String,
+      substringBody)
+    methodRegistry.register(
+      t.String, 'substring', [t.Int], t.String,
+      substringBody)
+
+    methodRegistry.register(
+      t.String, '+', [t.String], t.String,
+      ({receiver}, operand) => receiver + operand)
   }
 }
