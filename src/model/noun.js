e35f6249ad3ed9ecdf3025449c967fdfee276a0b
Ash Wilson <smashwilson@github.com>
2020-08-26 16:17:12 -0400

    Gnomish methods for Noun

--- src/model/noun.js
+++ src/model/noun.js
@@ -1,3 +1,5 @@
+const { makeType } = require('../gnomish/type')
+
 class Noun {
   constructor (room, name) {
     this.room = room
@@ -22,9 +24,32 @@ class Noun {
   }
 
   static registerMethods (t, symbolTable, methodRegistry) {
+    const tR = makeType("'R")
+    const tBlockR = makeType(t.Block, [tR])
+    const tStringList = makeType(t.List, [t.String])
+
     methodRegistry.register(
       t.Noun, 'getName', [], t.String,
       ({ receiver }) => receiver.getName()
+    )
+
+    methodRegistry.register(
+      t.Noun, 'verb', [t.String, tBlockR], t.Noun,
+      ({ receiver }, verb, block) => receiver.defineVerb(verb, block)
+    )
+    methodRegistry.register(
+      t.Noun, 'verb', [tStringList, tBlockR], t.Noun,
+      ({ receiver }, verbs, block) => {
+        for (const verb of verbs) {
+          receiver.defineVerb(verb, block)
+        }
+        return receiver
+      }
+    )
+
+    methodRegistry.register(
+      t.Noun, 'deleteVerb', [t.String], t.Bool,
+      ({ receiver }, verb, block) => receiver.deleteVerb(verb, block)
     )
   }
 }
